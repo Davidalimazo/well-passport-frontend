e60114365c052b6fa45a09c1db58e01ba079b8c5
Alimazoya David <davidalimazo@gmail.com>
2023-06-02 20:05:46 +0100

    adjusted homepage

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/home">
         <Layout>
           <App />
         </Layout>
