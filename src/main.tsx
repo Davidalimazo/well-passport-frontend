81299559a5295dfc14b46800405e876d8b4a8e72
Alimazoya David <davidalimazo@gmail.com>
2023-06-02 16:48:29 +0100

    added route basename

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Layout>
           <App />
         </Layout>
