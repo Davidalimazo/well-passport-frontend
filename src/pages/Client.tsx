88bb628dc08df82f82c5c6b442e8fe263a881758
Alimazoya David <davidalimazo@gmail.com>
2023-06-03 15:11:27 +0100

    added log

--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -58,9 +58,11 @@ const ClientListUi = () => {
         },
       });
       if (req.data.length > 0) {
+        console.log(req.data);
         const resData = req.data.map((item: any) => {
           return { ...item, image: item?.image?.split("\\")[1] };
         });
+        console.log(resData);
         setCachedDta(resData);
       }
     };
