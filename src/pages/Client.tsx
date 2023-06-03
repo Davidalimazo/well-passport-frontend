a26bf4d89b156c75f99c5decdd8cc3602892fdc7
Alimazoya David <davidalimazo@gmail.com>
2023-06-03 16:00:28 +0100

    adjusted image for vercel deployment

--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -34,7 +34,6 @@ export interface ClientDataProp {
 }
 
 export const imageUrlChecker = (url: string | undefined) => {
-  console.log(UPLOADS, url);
   console.log(UPLOADS + url);
   if (url) {
     return UPLOADS + url;
